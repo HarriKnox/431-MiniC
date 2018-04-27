93d8ddd1385773b445dac70ed670da9afa8b520d
Harrison Knox <hknox@protonmail.com>
2018-04-27 02:36:53 -0700

    Move FalseExp and TrueExp into BoolExp

---
+++ ast/expression/BoolExpression.java
@@ -0,0 +1,16 @@
+package ast.expression;
+
+
+public class BoolExpression
+   extends Expression
+{
+   public final boolean value;
+   
+   
+   public BoolExpression(int lineNum, boolean value)
+   {
+      super(lineNum);
+      
+      this.value = value;
+   }
+}
