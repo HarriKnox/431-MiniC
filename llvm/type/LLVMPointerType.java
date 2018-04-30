24c6eee072401319b2df0453fd12b009aee39bba
Harrison Knox <hknox@protonmail.com>
2018-04-30 15:42:11 -0700

    Add an LLVM 'Pointer' type
    This is just an i8* pointer type, the type returned by `malloc` and used by `free`. Aside from those two instances, it's not used anywhere

---
+++ llvm/type/LLVMPointerType.java
@@ -0,0 +1,11 @@
+package llvm.type;
+
+
+public class LLVMPointerType extends LLVMType
+{
+   @Override
+   public boolean equivalent(LLVMType t)
+   {
+      return t instanceof LLVMVoidType;
+   }
+}
