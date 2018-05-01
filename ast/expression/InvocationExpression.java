041199dbddc102f9d839b8f7def3004de4169da9
Harrison Knox <hknox@protonmail.com>
2018-04-30 18:50:11 -0700

    Set the height to the max of the list

--- ast/expression/InvocationExpression.java
+++ ast/expression/InvocationExpression.java
@@ -15,9 +15,21 @@ public class InvocationExpression extends Expression
          String name,
          List<Expression> arguments)
    {
-      super(lineNum, 0);
+      super(lineNum, getMax(arguments));
 
       this.name = name;
       this.arguments = arguments;
+   }
+   
+   
+   private static int getMax(List<Expression> args)
+   {
+      int max = 0;
+      
+      for (Expression exp : args)
+         if (exp.height > max)
+            max = exp.height;
+      
+      return max;
    }
 }
