36e92e28637d7fdca94fa7b09ee947e5fdc6fe04
Harrison Knox <hknox@protonmail.com>
2018-05-02 11:49:13 -0700

    Add meat to LLVMFunction

--- llvm/declaration/LLVMFunction.java
+++ llvm/declaration/LLVMFunction.java
@@ -3,4 +3,18 @@ package llvm.declaration;
 
 public class LLVMFunction
 {
+   public final String name;
+   public final LLVMType type;
+   public final List<LLVMParameter> parameters;
+   public final List<LLVMCFGNode> nodes;
+   
+   
+   public LLVMFunction(String name, LLVMType type,
+         List<LLVMParameter> parameters, List<LLVMCFGNode> nodes)
+   {
+      this.name = name;
+      this.type = type;
+      this.parameters = parameters;
+      this.nodes = nodes;
+   }
 }
