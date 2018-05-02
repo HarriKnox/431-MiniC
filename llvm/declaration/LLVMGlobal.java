6ea1a78b51db0895b301c9b654fb8ddc7fd3f06a
Harrison Knox <hknox@protonmail.com>
2018-05-02 10:16:16 -0700

    Add meat to LLVMGlobal and add the fields to LLVMStruct

--- llvm/declaration/LLVMGlobal.java
+++ llvm/declaration/LLVMGlobal.java
@@ -3,4 +3,13 @@ package llvm.declaration;
 
 public class LLVMGlobal
 {
+   public final String name;
+   public final LLVMType type;
+   
+   
+   public LLVMGlobal(name, type)
+   {
+      this.name = name;
+      this.type = type;
+   }
 }
--- llvm/declaration/LLVMStruct.java
+++ llvm/declaration/LLVMStruct.java
@@ -3,9 +3,15 @@ package llvm.declaration;
 
 import java.util.List;
 
+import llvm.type.LLVMType;
+
 
 public class LLVMStruct
 {
+   public final String name;
+   public final List<LLVMType> types;
+   
+   
    public LLVMStruct(String name, List<LLVMType> types)
    {
       this.name = name;
