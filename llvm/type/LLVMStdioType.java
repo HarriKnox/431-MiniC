560dab6f4ab741cede775f7512052865caa08c36
Harrison Knox <hknox@protonmail.com>
2018-05-04 18:42:54 -0700

    Add a special type for printf and scanf

---
+++ llvm/type/LLVMStdioType.java
@@ -0,0 +1,25 @@
+package llvm.type;
+
+
+public class LLVMStdioType extends LLVMType
+{
+   @Override
+   public boolean equivalent(LLVMType t)
+   {
+      return (t instanceof LLVMStdioType);
+   }
+   
+   
+   @Override
+   public String astString()
+   {
+      return "";
+   }
+   
+   
+   @Override
+   public String llvmString()
+   {
+      return "i32 (i8*, ...)";
+   }
+}
