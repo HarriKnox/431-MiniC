8940b67d3dce0c3230002fa5960d36b0dbd17b38
Harrison Knox <hknox@protonmail.com>
2018-05-05 23:46:45 -0700

    Add a bunch of options
    As well as documentation

--- common/Options.java
+++ common/Options.java
@@ -1,17 +1,87 @@
 package common;
 
+
+import java.io.File;
+
+
 public class Options
 {
-   public final String filename;
+   /**
+    * The name of the file (minus the '.mini'). To be used for naming the
+    * output files. If no source file is specified this defaults to 'a.mini'
+    * (to produce an 'a.out').
+    */
+   public final String name;
+   
+   
+   /**
+    * The source file. If it's null, then the input file is standard-in. This
+    * will be null if either no source file is presented or the source filename
+    * given is literally '-'.
+    *
+    *    minic -
+    */
+   public final File miniFile;
+   
+   
+   
+   /**
+    * Use the stack for storing variables.
+    */
    public final boolean stack;
-   public final boolean m32;
+   
+   
+   /**
+    * Prevents the compiler from cleaning up the control flow graphs.
+    *
+    * Orphans are nodes with no predecessors: nodes that would never be
+    * executed unless a cosmic ray hit the processor at just the right moment
+    * and the right angle to cause the program counter to jump into an orphan.
+    */
+   public final boolean orphans;
+   
+   
+   /**
+    * Use clang to compile. Will output the LLVM to a file in tmp.
+    */
+   public final boolean clang;
+   
+   
+   /**
+    * Output LLVM and don't continue compiling unless otherwise specified (if
+    * the -arm option is given then continue compiling).
+    */
+   public final boolean llvm;
+   
+   
+   /**
+    * Output human-readable ARM and don't continue compiling unless otherwise
+    * specified
+    */
+   public final boolean arm;
+   
    
    
    Options(OptionsBuilder ob)
    {
-      this.filename = ob.filename;
-      this.stack = ob.stack;
-      this.m32 = ob.m32;
+      if ((ob.name == null) || (ob.name.equals("-")))
+      {
+         this.name = "a";
+      }
+      else
+      {
+         this.name = ob.name;
+         this.miniFile = new File(this.name + ".mini");
+      }
+      
+      
+      
+      this.stack   = ob.stack;
+      this.orphans = ob.orphans;
+      this.clang   = ob.clang;
+      this.llvm    = ob.llvm;
+      this.arm     = ob.arm;
+      
    }
    
    
@@ -25,34 +95,43 @@ public class Options
          String arg = args[i];
          int len = arg.length();
          
-         if (arg.equals("-stack"))
+         if ((len > 1) && (arg.charAt(0) == '-'))
          {
-            ob.stack = true;
-         }
-         else if (arg.equals("-m32"))
-         {
-            ob.m32 = true;
-         }
-         else if (arg.charAt(0) == '-')
-         {
-            System.err.println("unexpected option: " + arg);
-            System.exit(6);
+            if (arg.equals("-stack"))
+               ob.stack = true;
+            
+            else if (arg.equals("-orphans"))
+               ob.orphans = true;
+            
+            else if (arg.equals("-clang"))
+               ob.clang = true;
+            
+            else if (arg.equals("-llvm"))
+               ob.llvm = true;
+            
+            else if (arg.equals("-arm"))
+               ob.arm = true;
+            
+            else
+               ErrorPrinter.printOut("unexpected option: " + arg);
          }
          else if (ob.filename != null)
          {
-            System.err.println("too many files specified");
-            System.exit(7);
+            ErrorPrinter.printOut("too many files specified: " + arg);
          }
-         else if ((len <= 5) || !arg.endsWith(".mini"))
+         else if (!arg.equals("-") && !arg.endsWith(".mini"))
          {
-            System.err.println("must be a .mini file");
-            System.exit(8);
+            ErrorPrinter.printOut("source must be a .mini file: " + arg);
          }
          else
          {
-            ob.filename = arg.substring(0, len - 5);
+            ob.name = arg;
          }
       }
+      
+      
+      if (ErrorPrinter.getErrorCount() > 0)
+         System.exit(1);
       
       
       return ob.create();
@@ -61,9 +140,14 @@ public class Options
    
    private static class OptionsBuilder
    {
-      String filename = null;
-      boolean stack = false;
-      boolean m32 = false;
+      String name;
+      
+      boolean stack;
+      boolean orphans;
+      
+      boolean clang;
+      boolean llvm;
+      boolean arm;
       
       Options create()
       {
