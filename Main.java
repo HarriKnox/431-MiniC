60c63db3c97f150b745704befcee03aa54fb5214
Harrison Knox <hknox@protonmail.com>
2018-05-07 00:22:22 -0700

    Add comments describing what's happening

--- Main.java
+++ Main.java
@@ -11,13 +11,25 @@ public class Main
 {
    public static void main(String[] args)
    {
+      /* Get options from command line arguments */
       Options opts = Options.parseOptions(args);
       
+      
+      /* Parse the source file and get the AST */
       ProgramAST ast = ProgramParser.parseProgram(opts);
       
+      
+      /* Build the LLVM representation using stack-based variable storage */
       ProgramLLVM llvm = ast.buildLLVM(opts);
       
-      llvm.writeLLVM();
+      
+      /* Compile with Clang (if enabled) */
+      clangCompile(opts, llvm);
+      
+      
+      /* Write all the LLVM code to stdout */
+      llvm.writeLLVM(new PrintWriter(new OutputStreamWriter(System.out)));
+      
       
       System.exit(0);
    }
--- common/Options.java
+++ common/Options.java
@@ -76,7 +76,6 @@ public class Options
       }
       
       
-      
       this.stack   = ob.stack;
       this.orphans = ob.orphans;
       this.clang   = ob.clang;
