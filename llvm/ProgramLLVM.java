4665ef70ae7346d4b60e1517b424a6011c7542f1
Harrison Knox <hknox@protonmail.com>
2018-05-07 00:52:00 -0700

    Add print statements for the rest of the declarations
    The other declarations include the C stdlib and stdio functions (malloc, free, printf, and scanf), and the globals associated with them (the format strings and scratch space).

--- llvm/ProgramLLVM.java
+++ llvm/ProgramLLVM.java
@@ -10,6 +10,13 @@ import llvm.declaration.LLVMStructs;
 import common.Options;
 
 
+import static llvm.value.constant.LLVMStdio.PRINTLN_FORMAT;
+import static llvm.value.constant.LLVMStdio.PRINT_FORMAT;
+import static llvm.value.constant.LLVMStdio.SCANF_FORMAT;
+
+import static llvm.value.variable.LLVMGlobal.SCANF_SCRATCH;
+
+
 public class ProgramLLVM
 {
    public final LLVMStructs llvmStructs;
@@ -28,8 +35,31 @@ public class ProgramLLVM
    
    public void writeLLVM(PrintWriter printer)
    {
+      printer.println("target triple=\"i686\"");
+      printer.println();
+      
+      
       this.llvmStructs.writeLLVM(printer);
       this.llvmGlobals.writeLLVM(printer);
       this.llvmFunctions.writeLLVM(printer);
+      
+      
+      printer.println("declare i8* @malloc(i32)");
+      printer.println("declare void @free(i8*)");
+      printer.println("declare i32 @printf(i8*, ...)");
+      printer.println("declare i32 @scanf(i8*, ...)");
+      
+      
+      printer.print(PRINTLN_FORMAT.name);
+      printer.println(" = private unnamed_addr constant [4 x i8] c\"%d\\0A\\00\", align 1");
+      
+      printer.print(PRINT_FORMAT.name);
+      printer.println(" = private unnamed_addr constant [4 x i8] c\"%d \\00\", align 1");
+      
+      printer.print(SCANF_FORMAT.name);
+      printer.println(" = private unnamed_addr constant [3 x i8] c\"%d\\00\", align 1");
+      
+      printer.print(SCANF_SCRATCH.llvmString());
+      printer.println(" = common global i32 0, align 4");
    }
 }
