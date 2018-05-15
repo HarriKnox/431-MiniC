eaafb42222493d172af2a5ed9b9f8d60395f6a82
Harrison Knox <hknox@protonmail.com>
2018-05-15 16:20:27 -0700

    Add basic guts for ARMCFGNode

--- arm/ARMCFGNode.java
+++ arm/ARMCFGNode.java
@@ -6,5 +6,23 @@ import arm.instruction.ARMInstruction;
 
 public class ARMCFGNode
 {
+   public final List<ARMInstruction> instructions = new LinkedList<>();
+   public final List<ARMCFGNode> predecessors = new LinkedList<>();
+   public ARMCFGNode loopback = null;
    
+   public final int uid;
+   
+   
+   public ARMCFGNode(int uid)
+   {
+      this.uid = uid;
+   }
+   
+   
+   public ARMCFGNode add(ARMInstruction instruction)
+   {
+      this.instructions.add(instruction);
+      
+      return this;
+   }
 }
