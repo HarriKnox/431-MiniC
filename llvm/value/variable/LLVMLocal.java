90f6228cf882736acc0dc7cf6983780e8e9cf725
Harrison Knox <hknox@protonmail.com>
2018-05-16 05:04:34 -0700

    Add constructors with index field to LLVMLocal

--- llvm/value/variable/LLVMLocal.java
+++ llvm/value/variable/LLVMLocal.java
@@ -8,14 +8,18 @@ public class LLVMLocal extends LLVMVariable
 {
    public final String function;
    public final String identifier;
+   public final int index;
    
    
-   public LLVMLocal(String function, String identifier, LLVMType type)
+   public LLVMLocal(String function,
+         String identifier,
+         LLVMType type, int index)
    {
       super(type);
       
       this.function = function;
       this.identifier = identifier;
+      this.index = index;
    }
    
    
--- llvm/value/variable/LLVMParameter.java
+++ llvm/value/variable/LLVMParameter.java
@@ -6,8 +6,9 @@ import llvm.type.LLVMType;
 
 public class LLVMParameter extends LLVMLocal
 {
-   public LLVMParameter(String function, String identifier, LLVMType type)
+   public LLVMParameter(String function,
+         String identifier, LLVMType type, int index)
    {
-      super(function, "param." + identifier,type);
+      super(function, "param." + identifier, type, index);
    }
 }
