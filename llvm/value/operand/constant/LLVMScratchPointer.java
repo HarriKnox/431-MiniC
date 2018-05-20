60385a7e6cc6197099155cd7e3f4332fa0a02705
Harrison Knox <hknox@protonmail.com>
2018-05-20 00:46:12 -0700

    Add a scratch pointer
    This will make doing function calls easier. The issue lies in the fact that, aside from the scratch pointer, all arguments are LLVMOperand types (constant values or registers, but not variable names). The scratch pointer is the only one that doesn't follow the pattern, so we create a separate one here, with similar functionality to the one in LLVMGlobal, but this acts as an LLVMPointerType.

---
+++ llvm/value/operand/constant/LLVMScratchPointer.java
@@ -0,0 +1,47 @@
+package llvm.value.operand.constant;
+
+
+import llvm.type.LLVMPointerType;
+
+import llvm.value.variable.LLVMGlobal;
+
+import arm.ARMCFGNode;
+
+import arm.instruction.ARMMovt;
+import arm.instruction.ARMMovw;
+
+import arm.value.immediate.ARMGlobal;
+
+import arm.value.operand.ARMRegister;
+
+
+public class LLVMScratchPointer extends LLVMConstant
+{
+   private LLVMScratchPointer()
+   {
+      super(new LLVMPointerType(LLVMGlobal.SCANF_SCRATCH.type));
+   }
+   
+   
+   @Override
+   public String llvmString()
+   {
+      return LLVMGlobal.SCANF_SCRATCH.llvmString();
+   }
+   
+   
+   public static final LLVMScratchPointer SCANF_SCRATCH
+         = new LLVMScratchPointer();
+   
+   
+   @Override
+   public ARMRegister buildARM(ARMCFGNode node)
+   {
+      ARMMovw movw = new ARMMovw(new ARMGlobal(this.llvmString()));
+      ARMMovt movt = new ARMMovt(movw.target, movw.value);
+      
+      node.add(movw).add(movt);
+      
+      return movw.target;
+   }
+}
