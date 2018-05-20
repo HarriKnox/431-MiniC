23661a5b3837de889e047764b869061b9da05995
Harrison Knox <hknox@protonmail.com>
2018-05-20 01:46:58 -0700

    Add constructor for ARMMul with target, left, right

--- arm/instruction/ARMMul.java
+++ arm/instruction/ARMMul.java
@@ -6,11 +6,19 @@ import arm.value.operand.ARMRegister;
 
 public class ARMMul extends ARMInstruction
 {
+   public final ARMRegister target;
    public final ARMRegister left;
    public final ARMRegister right;
    
+   
    public ARMMul(ARMRegister left, ARMRegister right)
    {
+      this(new ARMRegister(), left, right);
+   }
+   
+   public ARMMul(ARMRegister target, ARMRegister left, ARMRegister right)
+   {
+      this.target = target;
       this.left = left;
       this.right = right;
    }
